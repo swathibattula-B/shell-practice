7735d9da9d5acbc1346473a4ea34a0bca1e5a42c
swathi <battulaswathi15407@gmail.com>
2026-01-29 21:57:00 +0530

    swathidoe

---
+++ practice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1="swathi"
+PERSON2="sravani"
+echo "Hello, $PERSON1!"
+echo "Welcome to bash scripting, $PERSON2."
+echo "my name is $PERSON1."
+echo "I am learning bash scripting $PERSON2."
+echo "$PERSON2 how are you feeling good."
+echo "I hope you are doing well $PERSON1"
