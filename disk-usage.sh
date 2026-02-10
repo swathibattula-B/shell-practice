93f71cb317a8fbbde94d3187e9b0bf4bbc1ddc05
swathi <battulaswathi15407@gmail.com>
2026-02-11 00:31:20 +0530

    done

--- disk-usage.sh
+++ disk-usage.sh
@@ -26,4 +26,4 @@ done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
 
-sh mail.sh "info@joindevops.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
+sh mail.sh "battulaswathi15407@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
